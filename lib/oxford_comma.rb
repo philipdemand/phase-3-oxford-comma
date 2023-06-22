64a4837d85d6165d60531f40731f7ddf6aa2f563
Philip Demand <philipdemand@yahoo.com>
2023-06-22 12:32:48 -0400

    commit tree f8922a1f4fa44f371f33da03068910cd810ac00e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  target_string = ""
+  new_array = array.join(", ")
+  newer = new_array.split(", ")
+  if newer.length == 1
+   target_string << newer[0]
+  elsif newer.length == 2
+    target_string << newer[0]
+    target_string << " " << "and" << " "
+    target_string << newer[1]
+  else
+    all_but_last = newer[0...-1]
+    all_but_last.each do |item|
+        target_string << item << ", "
+      end
+    target_string << "and" << " "
+    target_string << newer[-1]
+  end
+  target_string
 end
